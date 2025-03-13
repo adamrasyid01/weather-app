18f1baaa5a88783d5d17c6ec426a9736c803a185
Adam Rasyid <adamrasyid01@gmail.com>
2025-03-13 14:49:16 +0700

    feat : Layouting App

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import CustomSearch from './components/CustomSearch.vue'
-import bgImage from './assets/weather.jpg'
+import CustomSearch from './components/CustomSearch.vue';
+import bgImage from './assets/rainy.jpg';
+import WeatherPage from './components/WeatherPage.vue';
 
-const searchQuery = ref()
+const searchQuery = ref();
 
 const tes = (query: string) => {
   console.log("Search query:", query);
-}
-
+};
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col items-center justify-center bg-cover bg-center p-4"
-  :style="{ backgroundImage: `url(${bgImage})` }">
-    <h1 class="text-2xl font-semibold text-white mb-4">Weather Forecast</h1>
-    <CustomSearch v-model="searchQuery" @search="tes" />
+  <div class="min-h-screen bg-black bg-opacity-70 relative flex items-center justify-center">
+    <!-- Background Image -->
+    <img :src="bgImage" class="absolute top-0 left-0 w-full h-full object-cover opacity-50" alt="Weather Background" />
+
+
+    <!-- Main Container -->
+    <div class="relative border-8 border-gray-300  bg-cover bg-center backdrop-blur-lg shadow-lg rounded-2xl w-[90%] max-w-4xl p-6 text-white"
+      :style="{ backgroundImage: `url(${bgImage})` }">
+     
+      <WeatherPage /> 
+    </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/WeatherPage.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import Background from '../assets/rainy.jpg';
+</script>
+
+<template>
+    <div class="flex">
+
+        <div class="basis-2/3 ">
+            <!-- Header  -->
+            <div class="flex flex-col justify-between h-full">
+                <div class="text-end p-6"> 21 April 2025 | 13:00</div>
+                <div class="flex flex-col gap-3 p-6">
+                    <div class="text-end font-bold">Heavy Rain</div>
+                    <hr class=" border-gray-400 opacity-60">
+                    <div class="flex gap-4">
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                        <div>Kotak</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="md:basis-1/3 bg-red-200 flex-col items-center justify-center text-black p-4">
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+            <p>Informasi Tambahan</p>
+        </div>
+    </div>
+</template>
