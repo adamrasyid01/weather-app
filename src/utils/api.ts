fd07d238abad7b22858d12b8667156adafb73247
Adam Rasyid <adamrasyid01@gmail.com>
2025-10-26 17:55:12 +0700

    fix: Proxy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,12 @@
 import axios from "axios";
 
-const isProd = import.meta.env.PROD;
-
+// const isProd = import.meta.env.PROD;
 
 export const baseRegionInstance = axios.create({
-  baseURL: isProd
-    ? import.meta.env.VITE_PROVINSI_INDONESIA 
-    : "/api-proxy/api",
-  headers: {
-    Accept: "application/json",
-  },
+  baseURL: "/api-proxy/api",
+  headers: { Accept: "application/json" },
 });
+
 
 export const baseForecastInstance = axios.create({
   baseURL: import.meta.env.VITE_BMKG,
